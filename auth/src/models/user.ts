1903a76ba3d050de2d50b4d7f53a3951e1ab77e4
joisyousef <yousefelsrogy500@gmail.com>
2026-09-25 15:57:27 +0300

    Formatting JSON Properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -24,30 +24,36 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  
-}, {
-  toJSON:{}
-});
-
-userSchema.pre(
-  "save",
-  async function () {
-    if (this.isModified("password")) {
-      const hashed = await Password.toHash(this.get("password"));
-      this.set("password", hashed);
-    }
-    // done();
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
   },
 );
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  // done();
+});
 
 // Custom function built into a model
 userSchema.statics.build = (attrs: UserAttrs) => {
