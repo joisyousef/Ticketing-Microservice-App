7ecce37a492c3dfbd5ec0ea1320fe89fb1f07b01
joisyousef <yousefelsrogy500@gmail.com>
2026-09-25 18:12:10 +0300

    Implement signin route logic with user validation and JWT generation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,5 +1,9 @@
 import express, { type Request, type Response } from "express";
 import { body } from "express-validator";
+import { Jwt } from "jsonwebtoken";
+import { User } from "../models/user.js";
+import { Password } from "../services/password.js";
+import { BadRequestError } from "../errors/bad-request-error.js";
 import { validateRequest } from "../middlewares/validate-request.js";
 
 const router = express.Router();
@@ -14,7 +18,35 @@ router.post(
       .withMessage("You must supply a password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {},
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const exisitingUser = await User.findOne({ email });
+    if (!exisitingUser) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    const passwordsMatch = await Password.compare(
+      exisitingUser.password,
+      password,
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: exisitingUser.directModifiedPaths,
+        email: exisitingUser.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    // Store it on a session object
+    req.session = {
+      jwt: userJwt,
+    };
+    res.status(200).send(exisitingUser);
+  },
 );
 
 export { router as signinRouter };
